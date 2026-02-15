3c79d1fe488d4c95805692d649e7d18b887966e7
runo6180-dot <runo6180@gmail.com>
2026-02-15 12:06:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.getElementById("searchBtn").addEventListener("click", () => {
     return;
   }
 
-  // ← ここで初めて API_URL を作る
+  // ← ここで初めて API_URL を作る（これが正解）
   const API_URL = `https://script.google.com/macros/s/AKfycbyHe4gC1D8F8REOY1EBLpntB7ISxqT5ttdH83_ZA4l1cwQq0yUt3rBRJWpqcM4NoKTz/exec?id=${videoID}`;
 
   console.log("送信するID:", videoID);
