b9dcecc332e1a58739dc204853e86300700df4e9
runo6180-dot <runo6180@gmail.com>
2026-02-15 11:58:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbyHe4gC1D8F8REOY1EBLpntB7ISxqT5ttdH83_ZA4l1cwQq0yUt3rBRJWpqcM4NoKTz/exec";
+const API_URL = `https://script.google.com/macros/s/AKfycbyHe4gC1D8F8REOY1EBLpntB7ISxqT5ttdH83_ZA4l1cwQq0yUt3rBRJWpqcM4NoKTz/exec?id=${videoID}`;
 
 document.getElementById("searchBtn").addEventListener("click", async () => {
   const url = document.getElementById("urlInput").value.trim();
